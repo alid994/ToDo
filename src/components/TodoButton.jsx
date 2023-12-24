eff037c6078dc111843568f7676ddf9460d26bd8
alid994 <aljo_dozer@hotmail.com>
2023-12-24 15:38:38 +0100

    edit components

---
+++ src/components/TodoButton.jsx
@@ -0,0 +1,9 @@
+
+function TodoButton({ label }) {
+    return (
+        <button className='btn btn-primary w-100 mt-3'>{label}</button>
+
+    )
+}
+
+export default TodoButton;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,35 @@
+
+function TodoList({ setTodos, toggleTodo, todos }) {
+
+    function deleteTodos(id) {
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id)
+        })
+    }
+
+    return (
+        <div className='card m-3 p-5'>
+            <h1>ToDo List</h1>
+            <ul className='list-group p-3'>
+                {todos.length === 0 && 'No Todos'}
+                {todos.map(todo => {
+                    return (
+                        <li key={todo.id} className='list-group-item m-1'>
+                            <label>
+                                <input type="checkbox" className='m-2'
+                                    checked={todo.complited}
+                                    onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                                {todo.tittle}
+                            </label>
+                            <button onClick={() => deleteTodos(todo.id)} className='btn btn-danger m-3'>Delete</button>
+                        </li>
+                    )
+                })}
+
+
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
